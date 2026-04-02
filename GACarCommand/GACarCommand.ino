5a567c4464f498390ea3553be10d5c34d99bb4a8
HerrmannRyan <ryblakeherrmann@gmail.com>
2026-04-01 22:27:12 -0600

    Update Car Command
    Car command now handles inputs and RTC breakout without any Sleep. RTC will just turn board back on

--- GACarCommand/GACarCommand.ino
+++ GACarCommand/GACarCommand.ino
@@ -1,16 +1,14 @@
-/*  To start, we need to implement 3 things:
-    LCD Display, LoRa Integration, Deep Sleep 
-    LCD Display will be using a shifter, but eventually
-    that will be on its own breakout. LoRa will be used
-    for the radio and that will work with deep sleep
-    deep sleep will be used including a breakout allowing for 
-    it to use ~0.1μA of power including an interupt from a LoRa
-    signal. */
+/*  Full code for the Guardian Angel Car Command:
+    This code will handle the following functions, after turning on the device the 
+    arduino will hold itself to high. It will also check to see if it was the RTC that 
+    turned it back on or if it was done on startup. This will run the emergency command
+    as well as setting sleep through UI. We will be implementing 4 input controls. It
+    includes 3 buttons for prompts and timer setting. We implement I2C for less pinouts
+    on the LCD display as well as Neopixel LEDs, and RTC for timer handling.*/
 
 // Importing Necessary Libraries
 #include <Adafruit_LiquidCrystal.h>
 #include <Wire.h>
-#include <avr/sleep.h>
 #include <RTClib.h>
 #include <Adafruit_NeoPixel.h>
 
@@ -20,15 +18,16 @@
 #define EMERGENCY_LIGHT_2 5
 #define NUMPIXELS 16
 
-// Define Interrupt Pin
-#define RTC_INT_PIN 2
-
 // Define the Scroll Wheel
 #define SCROLL_PIN A0
 
 // Define Buttons
 #define BTN_YES A1
 #define BTN_NO A2
+#define BTN_KILL A3
+
+// Define KillPin
+#define KILL_SWITCH 4
 
 // Declare and start Backpack
 Adafruit_LiquidCrystal lcd(0);
@@ -41,7 +40,7 @@ Adafruit_NeoPixel pixels1(NUMPIXELS, EMERGENCY_LIGHT_1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel pixels2(NUMPIXELS, EMERGENCY_LIGHT_2, NEO_GRB + NEO_KHZ800);
 
 // Declare sleep time variables
-int days, hours, minutes, seconds;
+int days, hours, minutes;
 
 // Declare DateTime formats to use
 DateTime startTime;
@@ -50,7 +49,7 @@ DateTime targetTime;
 // For button pressing logic, create enum datatype
 enum ButtonPress {NONE, YES, NO};
 
-// Button Press Checking Logic
+// Button Press YES NO logic.
 ButtonPress readButtons()
 {
   if (digitalRead(BTN_YES) == LOW)
@@ -68,6 +67,13 @@ ButtonPress readButtons()
   return NONE;
 }
 
+/*-----------------------------------------------------------
+  The following code handles the emergency scenario it will
+  function be simultaneously beeping and flashing red lights
+  on an interval. Emergency does that while the
+  ledLightSetup handles starting the rings. Important to wait
+  to set them up until later so that the current pull is low
+-----------------------------------------------------------*/
 // Emergency Alert Code
 void emergency()
 {
@@ -105,9 +111,25 @@ void emergency()
   delay(800);         // long pause before repeating
 }
 
-// Green Status Light Blink (Future?)
+// Setting up LED Ring Lights as RED
+void ledLightSetup()
+{
+  pixels1.begin();
+  pixels1.setBrightness(60);    //0 - 255
 
-// Timer selection Menu
+  pixels2.begin();
+  pixels2.setBrightness(60);    //0 - 255
+}
+
+/*-----------------------------------------------------------
+  The following code here is in charge of creating
+  a menu where the user can select the time that they
+  want the alarm to go off for, this is all UI so they can
+  go back and forth between menus as well as wait to 
+  start the timer until they are ready. Once ready, the
+  timer will be primed for the user to press the button kill
+  switch
+-----------------------------------------------------------*/
 // Create timer Menu
 void timeSelection()
 {
@@ -121,9 +143,9 @@ void timeSelection()
   bool inHourMenu = 1;
   bool inMinuteMenu = 0;
   int lastHours = -1;
-  int lastMinutes = -2;
+  int lastMinutes = -1;
 
-  // Create the hours and minutes to be multipied later
+  // Create the hours and minutes to be set on RTC
   int hourSelect = 0;
   int minuteSelect = 0;
 
@@ -192,40 +214,22 @@ void timeSelection()
         }
       }
     }
-
-    else if (btnHour == NO)
-    {
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Exiting");
-      
-      // No selection made
-      hourSelect = 0;
-      minuteSelect = 0;
-
-      // Escape Menu and loop
-      inHourMenu = 0;
-
-      // Allow user to see
-      delay(2000);
-      break;
-    }
   }
 
   hours = hourSelect;
   minutes = minuteSelect;
-  days = 0;
-  seconds = 0;
+
+  days = 0; // Days selection to be added in later
 }
 
 // Ask to start timer
-void timerStart()
+bool timerStart()
 {
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Start Sleep");
+  lcd.print("Set Alarm?");
   lcd.setCursor(0,1);
-  lcd.print("Timer? y/n");
+  lcd.print("Yes/No");
 
   while(1)
   {
@@ -233,12 +237,12 @@ void timerStart()
     if (btn == YES)
     {
       lcd.clear();
-      break;
+      return true;
     }
     else if (btn == NO)
     {
       lcd.clear();
-      while(1);
+      return false;
     }
   }
 }
@@ -274,87 +278,88 @@ void RTCSetup()
   rtc.disableAlarm(2);
 }
 
-void ledLightSetup()
-{
-  pixels1.begin();
-  pixels1.setBrightness(50);    //0 - 255
-
-  pixels2.begin();
-  pixels2.setBrightness(50);    //0 - 255
-}
-
-// Putting all to sleep
-void goToSleep() 
-{
-  // Attach interrupt on pin 2, wake on LOW signal from RTC alarm
-  attachInterrupt(digitalPinToInterrupt(RTC_INT_PIN), wakeUp, LOW);
-
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);  // Deepest sleep mode
-  sleep_enable();
-  sleep_mode();                         // Board sleeps HERE
-
-  // --- Wakes up here after interrupt ---
-  sleep_disable();
-  detachInterrupt(digitalPinToInterrupt(RTC_INT_PIN));
-
-  lcd.setBacklight(1);
-}
-
-void wakeUp() {
-  // ISR — intentionally empty, just wakes the board
-}
-
-// Sets the alarm and tells board to go to sleep also calling sleep command
-void setAlarmAndSleep() 
+// Sets the alarm and turns off power to main arduino board.
+void armAlarm() 
 {
   lcd.clear();
-  lcd.print("Sleeping...");
+  lcd.print("To set alarm,");
+  lcd.setCursor(0,1);
+  lcd.print("press ARM Button");
   delay(500);
-  lcd.clear();
-  lcd.setBacklight(0);
 
+  while(1)
+  {
+    if (digitalRead(BTN_KILL) == LOW)
+    {
+      break;
+    }
+  }
+
+  // Setup timer to get start and stop time
+  RTCSetup();
+
+  // Start Alarm
   rtc.setAlarm1(targetTime, DS3231_A1_Date);
-  goToSleep();
-  rtc.clearAlarm(1);  // clears AFTER waking
   delay(100);
+
+  // Kill power to board
+  digitalWrite(KILL_SWITCH, LOW);
 }
 
 void setup() 
 {
-  //Serial.begin(9600);
+  // For every start, check alarm and make kill pin high.
+  pinMode(KILL_SWITCH, OUTPUT);
+  digitalWrite(KILL_SWITCH, HIGH);
 
-  // Scroll wheel and button Setup
-  pinMode(BTN_YES, INPUT_PULLUP);
-  pinMode(BTN_NO, INPUT_PULLUP);
-
-  // Begin the setup of RTC Breakout
+  delay(100);
   rtc.begin();
 
   // Have LCD Initialize so that know is on
   lcd.begin(16,2);
   lcd.setBacklight(1);
-  lcdInitializationSetup();
-  //Serial.println("LCD Setup..");
 
-  timeSelection();
-  timerStart();
+  // If alarm fired, run emergency, if not run setup protocol
+  if(rtc.alarmFired(1))
+  {
+    // Clear Alarm after wake
+    rtc.clearAlarm(1);
 
-  RTCSetup();
+    // Setup Alarm Lights
+    ledLightSetup();
 
-  //Serial.println("RTC Setup...");
+    // Print Missing Person alert on LCD
+    lcd.print("MISSING PERSON:");
+    lcd.setCursor(0,1);
+    lcd.print("CALL S.A.R");
+    
+    while(1)
+    {
+      emergency();
+    }
+  }
+  else
+  {
+    // Scroll wheel and button Setup
+    pinMode(BTN_YES, INPUT_PULLUP);
+    pinMode(BTN_NO, INPUT_PULLUP);
+    pinMode(BTN_KILL, INPUT_PULLUP);
 
-  // Setup Alarm Lights
-  ledLightSetup();
+    lcdInitializationSetup(); // To be removed later this is validation testing so user can see it is on
 
-  // Start timer and go to sleep
-  setAlarmAndSleep();
-
-  lcd.print("EMERGENCY: MP ");
-  lcd.setCursor(0,1);
-  lcd.print("CALL SAR");
+    // Handle the menu logic for time selection
+    bool timerConfirmed = false;
+    while(!timerConfirmed)
+    {
+      timeSelection();
+      timerConfirmed = timerStart();
+    }
+    
+    // Start the hibernation and alarm for wake
+    armAlarm();
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  emergency();
 }
